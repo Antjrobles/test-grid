40dc2c7b8dc0c741ddbe6ef627e9308447adc78e
Antonio Robles <51507400+Antjrobles@users.noreply.github.com>
2023-06-01 16:45:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ const searchBtn = document.querySelector('.searchBtn');
 const searchBar = document.querySelector('.searchBar');
 const searchInput = document.getElementById('searchInput');
 const searchClose = document.getElementById('searchClose');
+const inputField = document.getElementById('searchInput')
+
 
 async function fetchImages() {
   cardsContainer.innerHTML = ''; // Limpiar el contenido existente
-
-  const perPage = 12; // Cambia el valor de perPage según tus necesidades
+  const perPage = 5;
+  // const searchParameter = inputText;
   
   try {
     const response = await fetch(
-      `https://api.pexels.com/v1/search?query=landscapes&per_page=${perPage}`,
+      `https://api.pexels.com/v1/search?query=landscape&per_page=${perPage}`,
       {
         headers: {
           Authorization: 'F5RU35VJ0hiSHmLcbiwhy9S5IjzIyJuDWByJRt2kwXs6qEY31oE9i955',
@@ -54,7 +56,6 @@ function createCard(photo) {
 
   const author = document.createElement('author');
   author.innerText = photo.photographer;
-  console.log(author);
 
   author.innerHTML = author.innerHTML === 'Pixabay' ? 'Stefan Keller' :
                   author.innerHTML === 'Nextvoyage' ? 'Antonio Robles' :
@@ -73,7 +74,7 @@ function createCard(photo) {
   return card;
 }
 
-// Search bar
+// SEARCH BAR
 searchBtn.addEventListener('click', function() {
   searchBar.style.visibility = 'visible';
   searchBtn.style.visibility = 'hidden'; 
@@ -86,16 +87,10 @@ searchClose.addEventListener('click', function() {
   searchBtn.style.visibility = 'visible';
   searchBar.style.visibility = 'hidden';
   searchBar.classList.remove('open');
-//   this.setAttribute('aria-expanded', 'false');
  });
 
 
-
-
-
-
-
-
+ // SHUFFLE FOTOS
 function shuffle(array) {
   let currentIndex = array.length,
     randomIndex;
@@ -107,8 +102,84 @@ function shuffle(array) {
       array[currentIndex]
     ];
   }
-
   return array;
 }
 
+
+// SEARCH INPUT
+inputField.addEventListener('keyup', (e) => {
+  const inputText = e.target.value;
+  fetchSearchImages(inputText);  
+})
+
 btnEl.addEventListener('click', fetchImages);
+
+
+
+
+
+//fetchSearchImages
+async function fetchSearchImages(inputText) {
+  cardsContainer.innerHTML = ''; // Limpiar el contenido existente
+  const perPage = 5;
+  const searchParameter = inputText;
+  
+  try {
+    const response = await fetch(
+      `https://api.pexels.com/v1/search?query=${searchParameter}&per_page=${perPage}`,
+      {
+        headers: {
+          Authorization: 'F5RU35VJ0hiSHmLcbiwhy9S5IjzIyJuDWByJRt2kwXs6qEY31oE9i955',
+        },
+      }
+    );
+    const data = await response.json();
+    const photos = data.photos;
+
+
+    photos.forEach((photo) => {
+      const card = createCard(photo);
+      cardsContainer.appendChild(card);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function createCard(photo) {
+  const card = document.createElement('div');
+  card.classList.add('cards');
+
+  const front = document.createElement('div');
+  front.classList.add('front');
+
+  const img = document.createElement('img');
+  img.src = photo.src.medium;
+  front.appendChild(img);
+
+  const back = document.createElement('div');
+  back.classList.add('back');
+
+  const text = document.createElement('textBack');
+  text.innerText = photo.alt;
+  back.appendChild(text);
+
+  const author = document.createElement('author');
+  author.innerText = photo.photographer;
+
+  author.innerHTML = author.innerHTML === 'Pixabay' ? 'Stefan Keller' :
+                  author.innerHTML === 'Nextvoyage' ? 'Antonio Robles' :
+                  author.innerHTML === 'katja' ? 'Katja Salenko' :
+                  author.innerHTML === 'Baskin Creative Studios' ? 'Mario Baskin' :
+                  author.innerHTML;
+
+  text.appendChild(author);
+  card.appendChild(front);
+  card.appendChild(back);
+
+  card.addEventListener('click', () => {
+    card.classList.toggle('flip');
+  });
+
+  return card;
+}
